6e76b7fe3e07456f1ae63582f13a340f41eb1634
Anastasiya <192827258+Anastasiya233@users.noreply.github.com>
2025-07-19 10:30:26 +0300

    ProjectAfter

---
+++ lib/app/bottom_nav_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppBottomNavBar extends StatelessWidget {
+  const AppBottomNavBar({required this.navigationShell, super.key});
+
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: navigationShell.currentIndex,
+        onTap: (index) => navigationShell.goBranch(
+          index,
+          initialLocation: index == navigationShell.currentIndex,
+        ),
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.store), label: 'Каталог'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Корзина',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Профиль'),
+        ],
+      ),
+    );
+  }
+}
